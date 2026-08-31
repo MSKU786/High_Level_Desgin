a580c870a77c559b756f5cfbe9310b41b7ffb8c4
MSKU786 <manishsingh332000@gmail.com>
2026-08-31 11:30:30 +0530

    Added basic project structure

---
+++ Circuit-Breakers/Posts/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.post("/", (req,res) => {
+
+})
+
+
+app.listen(PORT, () => {
+  console.log(`server listending on ${PORT}`);
+})
